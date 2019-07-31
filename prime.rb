43725a52949eea7a00a21f072deda1f1cec5fb1d
Broderick  Fowler Jr <broderickfowler@outlook.com>
2019-07-31 03:57:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,8 @@
 # Add  code here!
 def prime?(n)
-  puts "That's not an integer." unless n.is_a? Integer
-  is_prime = true
-  for i in 2..n-1
+ 
     if n % i == -1
-      is_prime = false
+
     end
   end
   if is_prime
